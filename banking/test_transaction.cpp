1a4d0ea3d0134c7bd065a06d491fa417c7814044
evalar4 <evelinka-07@mail.ru>
2025-05-27 00:13:58 +0300

    i forgot main directory

---
+++ banking/test_transaction.cpp
@@ -0,0 +1,103 @@
+#include "Transaction.h"
+#include "Account.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class MockAccount : public Account {
+public:
+    MockAccount(int id, int balance) : Account(id, balance) {}
+    
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
+};
+
+class MockTransaction : public Transaction {
+public:
+    MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
+};
+
+TEST(TransactionTest, MakeSuccess) {
+    MockAccount from(1, 200);
+    MockAccount to(2, 50);
+    Transaction tr;
+    tr.set_fee(10);
+
+    EXPECT_CALL(from, Lock()).Times(1);
+    EXPECT_CALL(from, Unlock()).Times(1);
+    EXPECT_CALL(to, Lock()).Times(1);
+    EXPECT_CALL(to, Unlock()).Times(1);
+    EXPECT_CALL(from, GetBalance()).WillOnce(testing::Return(200));
+    EXPECT_CALL(to, GetBalance()).WillOnce(testing::Return(50));
+
+    bool result = tr.Make(from, to, 100);
+    EXPECT_TRUE(result);
+}
+
+TEST(TransactionTest, MakeInvalidSameAccount) {
+    MockAccount acc(1, 100);
+    Transaction tr;
+    EXPECT_THROW(tr.Make(acc, acc, 50), std::logic_error);
+}
+
+TEST(TransactionTest, MakeInvalidNegativeSum) {
+    MockAccount from(1, 100);
+    MockAccount to(2, 50);
+    Transaction tr;
+    EXPECT_THROW(tr.Make(from, to, -10), std::invalid_argument);
+}
+
+TEST(TransactionTest, MakeInvalidTooSmallSum) {
+    MockAccount from(1, 100);
+    MockAccount to(2, 50);
+    Transaction tr;
+    EXPECT_THROW(tr.Make(from, to, 99), std::logic_error);
+}
+
+TEST(TransactionTest, MakeFeeTooHigh) {
+    MockAccount from(1, 200);
+    MockAccount to(2, 50);
+    Transaction tr;
+    tr.set_fee(60); // Для суммы 100 fee*2 = 120 > 100
+    
+    bool result = tr.Make(from, to, 100);
+    EXPECT_FALSE(result);
+}
+
+TEST(TransactionTest, DebitSuccess) {
+    MockAccount acc(1, 100);
+    Transaction tr;
+    
+    EXPECT_CALL(acc, GetBalance()).WillOnce(testing::Return(100));
+    bool result = tr.Debit(acc, 50);
+    EXPECT_TRUE(result);
+}
+
+TEST(TransactionTest, DebitFail) {
+    MockAccount acc(1, 100);
+    Transaction tr;
+    
+    EXPECT_CALL(acc, GetBalance()).WillOnce(testing::Return(100));
+    bool result = tr.Debit(acc, 150);
+    EXPECT_FALSE(result);
+}
+
+TEST(TransactionTest, Credit) {
+    MockAccount acc(1, 100);
+    Transaction tr;
+    
+    EXPECT_NO_THROW(tr.Credit(acc, 50));
+}
+
+TEST(TransactionTest, SaveToDatabaseMock) {
+    MockAccount from(1, 200);
+    MockAccount to(2, 50);
+    MockTransaction tr;
+    tr.set_fee(10);
+
+    EXPECT_CALL(tr, SaveToDataBase(testing::_, testing::_, testing::_)).Times(1);
+    
+    tr.Make(from, to, 100);
+}
+
