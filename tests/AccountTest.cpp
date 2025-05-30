47013eba97a24ee83bcd6900b12ad5b32e510287
evalar4 <151589441+evalar4@users.noreply.github.com>
2025-05-30 18:42:08 +0300

    new AccountTest.cpp

--- tests/AccountTest.cpp
+++ tests/AccountTest.cpp
@@ -6,6 +6,14 @@ TEST(AccountTest, GetBalanceReturnsInitialValue) {
     ASSERT_EQ(acc.GetBalance(), 1000);
 }
 
+TEST(AccountTest, ChangeBalanceRequiresLock) {
+    Account acc(1, 1000);
+    ASSERT_THROW(acc.ChangeBalance(100), std::runtime_error);
+    acc.Lock();
+    ASSERT_NO_THROW(acc.ChangeBalance(100));
+    ASSERT_EQ(acc.GetBalance(), 1100);
+}
+
 TEST(AccountTest, UnlockDoesNotThrowWhenUnlocked) {
     Account acc(1, 100);
     ASSERT_NO_THROW(acc.Unlock()); 
