c26aa51d68dc3993c38169cea314c0d03157d9c7
evalar4 <151589441+evalar4@users.noreply.github.com>
2025-05-30 18:34:54 +0300

    Update Transaction.h

--- banking/Transaction.h
+++ banking/Transaction.h
@@ -1,21 +1,19 @@
 #pragma once
 
-class Account;  // Forward declaration
+class Account;
 
 class Transaction {
  public:
   Transaction();
-  virtual ~Transaction();
+  ~Transaction();
 
   bool Make(Account& from, Account& to, int sum);
   int fee() const { return fee_; }
   void set_fee(int fee) { fee_ = fee; }
 
- protected:
-  void Credit(Account& accout, int sum);
-  bool Debit(Account& accout, int sum);
-
-  virtual void SaveToDataBase(Account& from, Account& to, int sum);
-
+ private:
+  void Credit(Account& account, int sum);  
+    bool Debit(Account& account, int sum);   
+    void SaveToDataBase(Account& from, Account& to, int sum);
   int fee_;
 };
